2e5cfa14eb241531301ba6241c1630aedc4b816d
Tancy-ux <tanushree@wareinnovations.com>
2026-05-27 18:10:02 +0530

    Initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import {Box} from "lucide-react";
+import Button from "./ui/Button";
+import {useOutletContext} from "react-router";
+
+const Navbar = () => {
+  const {isSignedIn, signIn, signOut, userName} = useOutletContext<AuthContext>()
+  const handleAuthClick = async () => {
+    if (isSignedIn) {
+      try {
+        await signOut();
+      } catch (e) {
+        console.error(`Puter sign out failed: ${e}`);
+      }
+      return;
+    }
+    try {
+      await signIn();
+    } catch (e) {
+      console.error(`Puter sign in failed: ${e}`);
+    }
+  }
+  return (
+    <header className="navbar">
+      <nav className="inner">
+        <div className="left">
+          <div className="brand">
+            <Box className="logo"/>
+            <span className="name">Roomify</span>
+          </div>
+          <ul className="links">
+            <a href="#">Product</a>
+            <a href="#">Pricing</a>
+            <a href="#">Community</a>
+            <a href="#">Enterprise</a>
+          </ul>
+        </div>
+        <div className="actions">
+          {isSignedIn ? (
+            <>
+            <span className="greeting">
+              {userName ? `Hi, ${userName}` : 'Signed In'}
+            </span>
+              <Button size="sm" onClick={handleAuthClick} className="btn">Log out</Button>
+            </>
+          ) : (
+
+            <>
+              <Button size="sm" variant="ghost" onClick={handleAuthClick} className="login">Log in</Button>
+              <a href="#upload" className="cta">Get Started</a>
+            </>
+          )}
+        </div>
+      </nav>
+    </header>
+  )
+}
+export default Navbar
